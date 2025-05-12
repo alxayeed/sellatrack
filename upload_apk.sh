f06d556c7dde61d8cd00bcd4b17a49784b396e2d
alxayeed <alxayeed@gmail.com>
2025-05-12 23:37:09 +0600

    ADD: firebase app distribution

---
+++ upload_apk.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#example file name - upload_apk.sh
+
+flutter build apk --release --split-per-abi
+
+APK_PATH="build/app/outputs/flutter-apk/app-arm64-v8a-release.apk"
+APP_ID="1:561909823794:android:53dd1f51f42419496cbf2b"
+RELEASE_NOTES_FILE_PATH="release-notes.txt"
+TESTERS_GROUP="al"
+
+# Step 3: Upload the APK to Firebase App Distribution with additional details
+firebase appdistribution:distribute $APK_PATH --app $APP_ID \
+    --release-notes-file $RELEASE_NOTES_FILE_PATH \
+    --groups "$TESTERS_GROUP" \
+
+
+#TESTERS_EMAILS="alxayeed@gmail.com"
+#firebase appdistribution:distribute $APK_PATH --app $APP_ID \
+#    --release-notes "Test release note" \
+#    --testers "$TESTERS_EMAILS"
