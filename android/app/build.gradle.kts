9f592cd010d9e90ee1a79d54740563c6eed6b945
alxayeed <alxayeed@gmail.com>
2025-05-25 17:03:36 +0600

    Release: 5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
+        versionCode = 5
         versionName = "0.1.0"
     }
 
